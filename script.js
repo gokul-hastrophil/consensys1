6039b99b808a0e4b761355c24c78c9141894a6eb
gokul-hastrophil <gokulprasanth63808@gmail.com>
2023-07-26 09:32:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+funtion_by_ID(){
+    alert("Input text");
+    console.log("Input text");
+    data = document.getElementById("text").value;
+    document.getElementById("para1").innerHTML = "javascript";
+}
